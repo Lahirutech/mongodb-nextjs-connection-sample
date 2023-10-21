947747363d06ac3144ddfa55110e58f1915edcd3
lanka <lanka@symbiote.com.au>
2023-10-21 14:29:09 +1100

    update the connection function

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -6,22 +6,27 @@ global.mongoose = {
 };
 
 export async function dbConnect() {
-  if (global.mongoose && global.mongoose.conn) {
-    console.log('connected from previous');
-    return global.mongoose.conn;
-  } else {
-    const conString = process.env.MONGO_URL;
+  try {
+    if (global.mongoose && global.mongoose.conn) {
+      console.log('Connected from previous');
+      return global.mongoose.conn;
+    } else {
+      const conString = process.env.MONGO_URL;
 
-    const promise = mongoose.connect(conString, {
-      autoIndex: true,
-    });
+      const promise = mongoose.connect(conString, {
+        autoIndex: true,
+      });
 
-    global.mongoose = {
-      conn: await promise,
-      promise,
-    };
-    console.log('Newly connected');
+      global.mongoose = {
+        conn: await promise,
+        promise,
+      };
 
-    return await promise;
+      console.log('Newly connected');
+      return await promise;
+    }
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    throw new Error('Database connection failed');
   }
 }
