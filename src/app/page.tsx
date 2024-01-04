50b785b0ef7a3f5642fa476a140fcaa87b9e2a78
Lanka Rathnayaka <rmllbrathnayaka@gmail.com>
2024-01-04 18:49:56 +1100

    adding the responsive styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return <>Home Page Sample</>;
+  return (
+    <>
+      Home Page Sample Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Odit veniam enim itaque aliquam amet ipsum obcaecati ad? Quo, id
+      aspernatur nam eveniet iure tempore ratione eaque sunt voluptatibus
+      consequatur laboriosam.lore Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Provident nemo doloribus aspernatur vero inventore nam,
+      ullam culpa quibusdam alias quisquam neque beatae ea autem voluptatem
+      doloremque, enim vitae aut expedita! Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Iusto, ratione ex molestiae placeat aliquid
+      iure numquam deleniti, expedita nemo, porro iste nesciunt enim id facere
+      natus hic pariatur vitae consequatur?
+    </>
+  );
 }
