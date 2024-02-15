9a0a5b03165d39bfc0bc83fd59e4738d58c14c8b
Lanka Rathnayaka <rmllbrathnayaka@gmail.com>
2024-02-15 19:31:14 +1100

    initialize categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Featured from "@/components/featured/Featured";
+import { Category } from "@/components/category/Category";
 
 export default function Home() {
   return (
     <>
       <Featured />
+      <Category />
     </>
   );
 }
---
+++ src/components/category/Category.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./category.module.css";
+import Link from "next/link";
+import nature from "@public/assets/images/nature.jpg";
+import Image from "next/image";
+
+export const Category = () => {
+  const categories=[{id:1,name:"Articles",bg:"article"}]
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Categories</h1>
+      <div className={styles.categories}>
+        <Link href={""} className={`${styles.category} ${styles.fashion}`}>
+          <Image
+            src={nature}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.caterImage}
+          />
+          Articls
+        </Link>
+        <Link href={""} className={`${styles.category} ${styles.fashion}`}>
+          <Image
+            src={nature}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.caterImage}
+          />
+          Articles
+        </Link>
+        <Link href={""} className={`${styles.category} ${styles.fashion}`}>
+          <Image
+            src={nature}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.caterImage}
+          />
+          Articls
+        </Link>
+        <Link href={""} className={`${styles.category} ${styles.fashion}`}>
+          <Image
+            src={nature}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.caterImage}
+          />
+          Articls
+        </Link>
+      </div>
+    </div>
+  );
+};
