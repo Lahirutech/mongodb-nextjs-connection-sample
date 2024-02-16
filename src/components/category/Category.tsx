c0d332224e48ef84adf393c8e251ffe7d533ed6e
Lanka Rathnayaka <rmllbrathnayaka@gmail.com>
2024-02-16 16:18:02 +1100

    add styles categories

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -5,7 +5,7 @@ import nature from "@public/assets/images/nature.jpg";
 import Image from "next/image";
 
 export const Category = () => {
-  const categories=[{id:1,name:"Articles",bg:"article"}]
+  const categories = [{ id: 1, name: "Articles", bg: "article" }];
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Categories</h1>
@@ -29,6 +29,16 @@ export const Category = () => {
             className={styles.caterImage}
           />
           Articles
+        </Link>
+        <Link href={""} className={`${styles.category} ${styles.fashion}`}>
+          <Image
+            src={nature}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.caterImage}
+          />
+          Articls
         </Link>
         <Link href={""} className={`${styles.category} ${styles.fashion}`}>
           <Image
